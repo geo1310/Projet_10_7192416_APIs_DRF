664d7e1039e75655a84816975a8f2ac66a940ba9
geo1310 <gbriche59@yahoo.fr>
2024-03-06 16:01:51 +0100

    Imbrication des serializers + pagination

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,16 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from shop.models import Category, Product, Article
 
 
-class CategorySerializer(ModelSerializer):
-
-    class Meta:
-        model = Category
-        fields = ["id", "name", "description", "date_created", "date_updated", "active"]
-
-
 class ProductSerializer(ModelSerializer):
+
+    articles = SerializerMethodField()
 
     class Meta:
         model = Product
@@ -22,7 +17,36 @@ class ProductSerializer(ModelSerializer):
             "date_created",
             "date_updated",
             "active",
+            "articles",
         ]
+
+    def get_articles(self, instance):
+        queryset = instance.articles.filter(active=True)
+        serializer = ArticleSerializer(queryset, many=True)
+        return serializer.data
+
+
+class CategorySerializer(ModelSerializer):
+
+    # En utilisant un `SerializerMethodField', il est nécessaire d'écrire une méthode
+    # nommée 'get_XXX' où XXX est le nom de l'attribut, ici 'products'
+    products = SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ["id", "date_created", "date_updated", "name", "products"]
+
+    def get_products(self, instance):
+        # Le paramètre 'instance' est l'instance de la catégorie consultée.
+        # Dans le cas d'une liste, cette méthode est appelée autant de fois qu'il y a
+        # d'entités dans la liste
+
+        # On applique le filtre sur notre queryset pour n'avoir que les produits actifs
+        queryset = instance.products.filter(active=True)
+        # Le serializer est créé avec le queryset défini et toujours défini en tant que many=True
+        serializer = ProductSerializer(queryset, many=True)
+        # la propriété '.data' est le rendu de notre serializer que nous retournons ici
+        return serializer.data
 
 
 class ArticleSerializer(ModelSerializer):
